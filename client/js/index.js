2833a3d8ce2275f5ba2a4e8a232e0fad2b9d7c4b
Vidith <vidhmadh1892@gmail.com>
2022-04-03 13:03:59 -0500

    added sample map interface

---
+++ client/js/index.js
@@ -0,0 +1,43 @@
+/* OSM & OL example code provided by https://mediarealm.com.au/ */
+var map;
+var mapLat = -33.829357;
+var mapLng = 150.961761;
+var mapDefaultZoom = 10;
+
+function initialize_map() {
+  map = new ol.Map({
+    target: "map",
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM({
+                  url: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            })
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([mapLng, mapLat]),
+        zoom: mapDefaultZoom
+    })
+  });
+}
+
+function add_map_point(lat, lng) {
+  var vectorLayer = new ol.layer.Vector({
+    source:new ol.source.Vector({
+      features: [new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
+        })]
+    }),
+    style: new ol.style.Style({
+      image: new ol.style.Icon({
+        anchor: [0.5, 0.5],
+        anchorXUnits: "fraction",
+        anchorYUnits: "fraction",
+        src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg"
+      })
+    })
+  });
+
+  map.addLayer(vectorLayer); 
+}
+

e47ca7786133e3c8e0907cb00003bcb93b63f1b2
Jack Boller <jackboller1@gmail.com>
2022-04-17 12:58:32 -0500

    added crime standardization for nyc

--- city_info.py
+++ city_info.py
@@ -1,7 +1,9 @@
 url_group = {"Chicago" : "https://data.cityofchicago.org/resource/ijzp-q8t2.json?",
-            "Austin" : "https://data.austintexas.gov/resource/fdj4-gpfu.json?"
+            "Austin" : "https://data.austintexas.gov/resource/fdj4-gpfu.json?",
+            "New York" : "https://data.cityofnewyork.us/resource/uip8-fykc.json?"
             }
 
 date_field_group = {"Chicago" : "date",
-                    "Austin" : "occ_date"
+                    "Austin" : "occ_date",
+                    "New York" : "arrest_date"
                     }

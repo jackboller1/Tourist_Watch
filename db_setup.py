fd66fe4d1200ba1ad061e8c975d79b3363718072
Jack Boller <jackboller1@gmail.com>
2022-04-20 16:49:32 -0500

    proposed schema for collections and created file to store db operations

--- db_setup.py
+++ db_setup.py
@@ -8,25 +8,15 @@ DB_USERNAME = parse.quote_plus((os.environ.get("MONGODB_USERNAME")))
 DB_PASSWORD = parse.quote_plus((os.environ.get("MONGODB_PASSWORD")))
 CLUSTER_NAME= parse.quote_plus((os.environ.get("CLUSTER_NAME")))
 
-test_document = {
-    "name" : "J",
-    "city" : "Fort Worth",
-    "text" : "test"
-}
-
 def get_database():
-    #client = MongoClient(f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@csce315project3.jqwud.mongodb.net/testimonials?retryWrites=true&w=majority")
+    '''return instance of database'''
     client = MongoClient(f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@{CLUSTER_NAME}.jqwud.mongodb.net/testimonials?retryWrites=true&w=majority")
     db = client.csce315project3
     return db
 
-def insert_testimonial(test_collection, testimonial):
-    '''Insert user's testimonial into the testimonial collection'''
-    test_collection.insert_one(testimonial)
 
 db = get_database()
 testimonials = db.testimonials
 
-testimonials.insert_one(test_document)
-print(testimonials.find_one())
+
 
